b42ede23de496b6194a89740e096d542e0bc495f
Khalil Hussaini <Khalil.Hussaini@swisscom.com>
2025-11-07 14:45:32 +0100

    rebuild fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter , Router, Route, Routes } from "react-router-dom";
 import Header from "./pages/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -17,6 +17,7 @@ const App = () => {
   return (
     <Router>
       <Header />
+      <BrowserRouter basename="/Travel-Tour-Website-client">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -27,7 +28,7 @@ const App = () => {
           <Route path="BookingForm" element={<BookingForm />} />
         </Route>
       </Routes>
-
+      </BrowserRouter>
       <Footer />
       <UserContextProvider>
         <Outlet />
